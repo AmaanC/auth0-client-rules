90b145c41c48f1be15590cc0ed3a0d1ce0cde4e3
Amaan Cheval <amaan.cheval@gmail.com>
2016-07-27 19:57:25 +0530

    Use clientSecret as session secret too, instead of a hardcoded value

--- app.js
+++ app.js
@@ -51,7 +51,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(session({
-    secret: 'shhhhhhhhh',
+    secret: strategy.clientSecret,
     resave: true,
     saveUninitialized: true
 }));
